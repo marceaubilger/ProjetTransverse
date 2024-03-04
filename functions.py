1ff67d91e3fe142c756b941aa8fe59b14a60091f
marceaubilger <marceau.bilger@orange.fr>
2024-03-04 11:42:27 +0100

    update sprite

--- functions.py
+++ functions.py
@@ -1,20 +1,3 @@
 import pygame
-screen_info = pygame.display.Info()
-WIDTH, HEIGHT = screen_info.current_w, screen_info.current_h
+import math
 
-def load_screen():
-    sky_surface=pygame.image.load("C:\ProjetTransverse\layer-1-sky.png")
-    resized_sky = pygame.transform.scale(sky_surface, (WIDTH, HEIGHT-100))
-    ground=pygame.image.load("C:\ProjetTransverse\ground.png")
-    resized_ground = pygame.transform.scale(ground, (WIDTH,100))
-
-
-def arrow_rotation(angle):
-    arrow=pygame.image.load("C:\ProjetTransverse\pngwing.com.png")
-    resized_arrow=pygame.transform.scale(arrow,(200,100))
-    arrow_rect = resized_arrow.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-    if angle > 90 or angle < 0:
-        rotation_speed *= -1
-    angle += rotation_speed
-    rotated_arrow = pygame.transform.rotate(resized_arrow, angle)
-    rotated_rect = rotated_arrow.get_rect(center=arrow_rect.center)
--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import pygame
 import sys
 from pygame import QUIT
 import os
+import math
 
 #trouve le chemin d'acces pour les sprites pour pouvoir les utiliser
 script_path = os.path.abspath(sys.argv[0]).replace("main.py","") 
 nom_player="player_stand.png"
 chemin_player=os.path.join(script_path,nom_player)
-nom_sky="layer-1-sky.png"
+nom_sky="background.png"
 chemin_sky=os.path.join(script_path,nom_sky)
-nom_sol="ground.png"
+nom_sol="base_sol_3.png"
 chemin_sol=os.path.join(script_path,nom_sol)
 nom_arrow="pngwing.com.png"
 chemin_arrow=os.path.join(script_path,nom_arrow)
 nom_jauge="buttonLong_brown.png"
 chemin_jauge=os.path.join(script_path,nom_jauge)
+nom_montain="mountain.png"
+chemin_mountain=os.path.join(script_path,nom_montain)
+nom_sentier="sand_1.png"
+chemin_sentier=os.path.join(script_path,nom_sentier)
 
 pygame.font.init()
 font_test = pygame.font.Font(None, 36)  
@@ -25,10 +30,17 @@ WIDTH, HEIGHT = screen_info.current_w, screen_info.current_h #utilise une foncti
 
 sky_surface=pygame.image.load(chemin_sky)
 resized_sky = pygame.transform.scale(sky_surface, (WIDTH, HEIGHT-100)) #load le png du fond et le resize 
+resized_sky_rect=resized_sky.get_rect(topleft=(0,0))
+
+sentier=pygame.image.load(chemin_sentier)
+sentier_rect=sentier.get_rect(topleft=(0,420))
+
+mountain=pygame.image.load(chemin_mountain)
+mountain_rect=mountain.get_rect(topleft=(0,370))
 
 ground=pygame.image.load(chemin_sol)
-resized_ground = pygame.transform.scale(ground, (WIDTH,100))
-resized_ground_rect=resized_ground.get_rect(topleft=(0,HEIGHT-100)) # load le png du sol et le resize
+resized_ground = pygame.transform.scale(ground, (WIDTH,300))
+resized_ground_rect=resized_ground.get_rect(topleft=(0,HEIGHT-300)) # load le png du sol et le resize
 
 arrow=pygame.image.load(chemin_arrow)
 resized_arrow=pygame.transform.scale(arrow,(200,100))
@@ -37,7 +49,7 @@ arrow_activated=True
 
 
 player=pygame.image.load(chemin_player)
-player_rect=player.get_rect(bottomleft=(WIDTH//2,HEIGHT-300))
+player_rect=player.get_rect(bottomleft=(WIDTH//3,sentier_rect.top+265))
 
 jauge=pygame.image.load(chemin_jauge)
 jauge_rect=jauge.get_rect(bottomleft=(100,HEIGHT*2/3))
@@ -51,10 +63,11 @@ move_bar=15
 screen=pygame.display.set_mode((WIDTH,HEIGHT),pygame.FULLSCREEN)
 clock=pygame.time.Clock()
 
-gravity=5
+
 run=True
 angle = 0
-rotation_speed = 4
+rotation_speed = 10
+
 while run==True:
     clock.tick(60)
     keys=pygame.key.get_pressed()
@@ -66,10 +79,6 @@ while run==True:
                 run = False 
 
 
-
-    if keys[pygame.K_UP]:# fait une sorte de gravité, pas neccessaire en fonction de la future fonction trajectoire
-        player_rect.y-=15
-
     if keys[pygame.K_SPACE] and jauge_activated==True: # calcule la valeur de la jauge lorsque la touche aspace est pressée
         strenght_value=5-4*(bar_rect.y-jauge_rect.top)/(jauge_rect.bottom-jauge_rect.top)
         strenght_value=round(strenght_value, 2)
@@ -79,18 +88,12 @@ while run==True:
         angle_value=angle
         arrow_activated=False
 
-    player_rect.y+=gravity #applique la trajectoire au joueur
 
-    if player_rect.bottom>resized_ground_rect.top: #arrete la chute du joueur si il touche le sol
-        player_rect.bottom=resized_ground_rect.top
-    if player_rect.bottom==resized_ground_rect.top:
-        gravity=5
-    else:
-        gravity+=1
-
-    screen.blit(resized_sky,(0,0))
-    screen.blit(resized_ground,(0,HEIGHT-100))
-    screen.blit(player,player_rect)#update le display du joueur et du background
+    screen.blit(resized_sky,resized_sky_rect)
+    screen.blit(mountain,mountain_rect)
+    screen.blit(resized_ground,resized_ground_rect)
+    screen.blit(player,player_rect)
+    screen.blit(sentier,sentier_rect)#update le display du joueur et du background
 
     if jauge_activated:
         if(bar_rect.bottom==jauge_rect.bottom or bar_rect.top==jauge_rect.top):
